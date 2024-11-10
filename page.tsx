84e27fc296b325198856e06b0a4678cd2721e73b
Imran hassan <159555195+Imran-has@users.noreply.github.com>
2024-11-10 07:02:32 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,22 @@
+
+import Header from "./components/Headerr"
+import Hero from "./components/Herro"
+import Form from"./components/Form"
+import Footer from "./components/Footer"
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header/>
+      <Hero/>
+      <Form/>
+      
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default Homepage
+
